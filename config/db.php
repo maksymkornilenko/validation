052a41c0c5413f0630cd7a121d20979472505f2c
maksymkornilenko <kornilenko1993@gmail.com>
2020-08-31 18:28:17 +0200

    htaccess

--- config/db.php
+++ config/db.php
@@ -1,10 +1,20 @@
 <?php
 
+if (getenv("YII_ENV") == 'prod') {
+    $url = parse_url(getenv("DATABASE_URL"));
+    $dsn = 'pgsql:host='.$url['host'].';port='.$url['port'].';dbname='.substr($url["path"], 1);
+    $username = $url["user"];
+    $password = $url["pass"];
+} else {
+    $dsn = 'pgsql:host=localhost;port=5438;dbname=validation';
+    $username = 'postgres';
+    $password = '';
+}
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5438;dbname=validation',
-    'username' => 'postgres',
-    'password' => '',
+    'dsn' => $dsn,
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
